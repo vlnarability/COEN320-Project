86d53bb0335b6175830f724aa5729966a10f385c
lucatomassi <luca@tomassi.ca>
2021-12-06 19:21:00 -0500

    removed uncessary files

--- commandprocessor.cpp
+++
@@ -1,6 +0,0 @@
-/*
- * Commandprocessor.cpp
- * Sporadic task that receives user request, in order to update one or all producer.cpp tasks
- * 
- * 
-*/
--- producer.cpp
+++
@@ -1,7 +0,0 @@
-/*
- * Producer.cpp
- * Data Producer: One real-time task for EACH variable (5), parsed from the dataset provided to us
- * Each periodic task reads data from dataset. Thread-Timer to be used
- * 
- * 
-*/
