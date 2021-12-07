f4cfde4bab5f7abdeb099c04ee4413a498f33e3d
Linfeng266 <71446857+Linfeng266@users.noreply.github.com>
2021-12-07 16:33:00 -0500

    Update FileRead.h

--- FileRead.h
+++ FileRead.h
@@ -1,20 +1,18 @@
-#define _GNU_SOURCE
+#include <stdlib.h>
+#include <stdio.h>
+#include <stdint.h>
+#include <time.h>
+#include <sys/time.h>
+#include <pthread.h>
 #include <sys/types.h>
 #include <fcntl.h>
 #include <unistd.h>
-#include <stdlib.h>
-#include <stdio.h>
 #include <dirent.h>
-#include <pthread.h>
-#include <sys/time.h>
 #include <signal.h>
-#include <time.h>
-#include <stdint.h>
 #include <string.h>
-#include <string.h>
-#include <semaphore.h>
 #include <stdbool.h>
 #include <termios.h>
+#define _GNU_SOURCE
 
 #ifndef THREADFUNCTION_H_
 #define THREADFUNCTION_H_
@@ -25,16 +23,16 @@
 #define MAX_LINE_SIZE 9999
 #define MAX_SHEET_SIZE 9999
 
-typedef enum { F, T } boolean;
+typedef enum {F,T} boolean;
 extern boolean state;
 int frequency[5];// command frequency 
 //extern boolean command;// check command
 
-typedef struct {		//define buffer
-	float data[100];
+typedef struct{		//define buffer
+	float data[100];			
 }DataBuffer;
 
-typedef struct Param {
+typedef struct Param{
 	int period;//period in microseconds
 	int filenum;//1-Fuel_consumption, 2-Engine_speed, 3-Engine_coolant_temperature, 4-Current_Gear, 5-Vehicle_speed
 }Param;
@@ -47,5 +45,5 @@ void* file_read(DataBuffer*);		//read function
 
 void commandUpdate();
 
-int getline(char*, int);			//get command
+int getline(char*,int);			//get command
 #endif /*THREADFUNCTION_H_*/
